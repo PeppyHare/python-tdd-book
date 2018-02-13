3929c0bb0ded284affeb4a3415c24d518928c945
Evan <bluhme3@gmail.com>
2018-02-13 10:44:43 -0800

    Automated commit from passing tests

--- filewatcher.sh
+++ filewatcher.sh
@@ -3,5 +3,6 @@
 # This is just a wrapper for the filewatcher gem
 #  (https://github.com/thomasfl/filewatcher)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+GPG_TTY=$(tty)
+export GPG_TTY
 filewatcher '**/*.py **/*.sh **/templates/*.html' "bash $DIR/runtests.sh
