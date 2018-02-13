52baa54e77723ed0d86272f2e361454fa1daf8bd
Evan <bluhme3@gmail.com>
2018-02-13 09:54:17 -0800

    Adding venv for linux

--- yapify.sh
+++ yapify.sh
@@ -2,3 +2,7 @@
 
 # Just run yapf on stuff
 yapf -ipr superlists lists functional_tests
+1
+2
+PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+PATH="$PATH:/mnt/c/Program\ Files/Docker/Docker/resources/bin"
