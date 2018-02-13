10ab867826224b7d2384b6e2eba81fe4ff081fa8
Evan <bluhme3@gmail.com>
2018-02-13 13:50:00 -0800

    Automated commit from passing tests

--- filewatcher.sh
+++ filewatcher.sh
@@ -5,4 +5,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GPG_TTY=$(tty)
 export GPG_TTY
-filewatcher -s -I '**/*.py **/*.sh **/*.html **/*.js' "echo \"$FILENAME has changed\" && bash $DIR/runtests.sh"
+filewatcher -s '**/*.py **/*.sh **/*.html **/*.js' "echo $FILENAME && bash $DIR/runtests.sh"
