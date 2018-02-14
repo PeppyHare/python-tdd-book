7976fa6403ae11407c437317f263e33f990c3ee0
Evan <bluhme3@gmail.com>
2018-02-14 11:01:33 -0800

    Automated commit from passing tests

--- superlists/settings.py
+++ superlists/settings.py
@@ -128,3 +128,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # TEST_RUNNER = 'superlists.test_runner.MyTestRunner'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'obeythetestinggoat@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
