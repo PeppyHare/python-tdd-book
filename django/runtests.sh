81dbfbc76e0ed6118cff646cdca127f425d0eb45
Evan Bluhm <bluhme3@gmail.com>
2018-02-24 10:09:17 -0800

    Automated commit from passing tests. Now on https://www.obeythetestinggoat.com/book/chapter_server_side_debugging.html

--- django/runtests.sh
+++ django/runtests.sh
@@ -3,17 +3,17 @@
 # The object of this is to
 #  - format all source files with https://github.com/google/yapf
 #  - quickly run all specified tests when a file changes
+#  - push the code to some staging environment and run FT against it
 #  - create a new git commit if all of the tests are passing :white_check_mark:
 # 
-# In general, while working on the project, I use https://github.com/cespare/reflex to run the tests passively while I make changes. From this working directory, I can run:
+# In general, while working on the project, I use http://entrproject.org/ to run the tests passively while I make changes. From this working directory, I can run:
 # 
-# $ reflex bash runtests.sh
+# $ ag -l | entr -d bash "$DIR/runtests.sh"
 # 
 # This will watch for any file modifications in the project, and re-run the tests (and possibly commit the code) when they occur (wow such TDD)
 # 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CURRENT_CHAPTER_LINK="https://www.obeythetestinggoat.com/book/chapter_server_side_debugging.html"
-# export STAGING_SERVER=superlists-staging.peppyhare.uk
 
 # startPyVenv() {
 #     source "$DIR/venv/bin/activate"
@@ -27,40 +27,53 @@ testSuperlists() {
 }
 
 formatCode() {
-	cd "$DIR" || exit
-	printf "\033[32mApplying yapf...\033[0m\n"
+    cd "$DIR" || exit
+    printf "\033[32mApplying yapf...\033[0m\n"
     python -m yapf -i -r ./superlists
     python -m yapf -i -r ./lists
     python -m yapf -i -r ./functional_tests
 }
 
+branchOff() {
+    cd "$DIR" || exit
+    git checkout dev
+    git add .
+    git status
+    git commit -m "Automated commit from passing tests. Now on $CURRENT_CHAPTER_LINK" && git push origin dev
+}
+
 fullTest() {
-	echo "Always fail"
-	return 1
+    echo "Always fail"
+    export STAGING_SERVER=superlists-staging.peppyhare.uk
+    return 1
 }
 
 commitCode() {
-	cd "$DIR" || exit
-	git add .
-	git status
-	git commit -m "Automated commit from passing tests. Now on $CURRENT_CHAPTER_LINK" && git push
-	printf "\033[32mEverything's looking good :)\033[0m\n\n"
-	return 0 # just in case nothing to commit
+    printf "\033[32mCommitting changes to the master branch\033[0m\n\n"
+    git checkout master
+    git pull origin dev
+    git push origin master
+    git checkout dev
+}
+
+fail() {
+    printf "\033[31mNot passing tests... :(\033[0m\n"
+    exit 1
+}
+
+success() {
+    printf "\033[32mEverything's looking good :)\033[0m\n\n"
+    return 0
 }
 
 echo ""
-echo "$(date) :  Testing out new changes now :)"
+printf "\033[32m$(date) :  Testing out new changes now :)\033[0m\n"
 
-testSuperlists
-STATUS1=$?
-fullTest
-STATUS2=$?
-STATUS=$(($STATUS1+$STATUS2))
-echo "Final status: $STATUS"
-if [[ $STATUS == "0" ]]; then
-    printf "\033[32mPassing tests!\033[0m\n"
-    formatCode
-	commitCode
+testSuperlists || fail
+formatCode
+branchOff || fail
+fullTest || fail
+commitCode
 else
-	printf "\033[31mNot passing tests... :(\033[0m\n"
+    printf "\033[31mNot passing tests... :(\033[0m\n"
 fi
