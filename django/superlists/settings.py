c2b279d39ae25818aef9d1f00e47de1d9c79931e
Evan Bluhm <bluhme3@gmail.com>
2018-02-24 10:16:57 -0800

    Automated commit from passing tests. Now on https://www.obeythetestinggoat.com/book/chapter_server_side_debugging.html

--- django/superlists/settings.py
+++ django/superlists/settings.py
@@ -90,24 +90,24 @@ DATABASES = {
 }
 
 # Custom logging settings
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'console': {
-#             'level': 'DEBUG',
-#             'class': 'logging.StreamHandler',
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['console'],
-#         },
-#     },
-#     'root': {
-#         'level': 'INFO'
-#     },
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {
+        'level': 'INFO'
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
