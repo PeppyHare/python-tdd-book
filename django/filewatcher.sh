7649fbd00f8e7e967d90db53b4e371a8ff35a437
Evan Bluhm <bluhme3@gmail.com>
2018-02-24 10:52:45 -0800

    Automated commit from passing tests. Now on https://www.obeythetestinggoat.com/book/chapter_server_side_debugging.html

--- django/filewatcher.sh
+++ django/filewatcher.sh
@@ -4,6 +4,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GPG_TTY=$(tty)
 export GPG_TTY
+cd "$DIR" || exit 1
+# source ../venv/bin/activate
 
 # Can also use `find` here if silversurfer not installed
 ag -l | entr -d bash "$DIR/runtests.sh"
