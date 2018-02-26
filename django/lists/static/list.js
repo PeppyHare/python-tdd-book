7039cb93ebe210b981e742b83a349ae939e9848f
Evan Bluhm <bluhme3@gmail.com>
2018-02-26 14:58:25 -0800

    Automated commit from passing tests. Now on https://www.obeythetestinggoat.com/book/chapter_server_side_debugging.html

--- django/lists/static/list.js
+++ django/lists/static/list.js
@@ -1,8 +1,8 @@
 window.Superlists = {}; 
 window.Superlists.initialize = function () { 
-  $('input[name="text"]').keypress(function () {
+  $('input[name="text"]').on('keypress', (function () {
   	// if ($(this).value) {
 	$('.has-error').hide();
     // }
-  });
+  }));
 };
