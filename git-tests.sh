c2c4b1b1bb1abf8f1e0cc9f1d0c6cf8c35e9d8af
Evan Bluhm <bluhme3@gmail.com>
2018-02-27 15:46:46 -0800

    Dummy commit

--- git-tests.sh
+++ git-tests.sh
@@ -14,10 +14,4 @@
 # 0.03user 0.15system 0:05.10elapsed 3%CPU (0avgtext+0avgdata 3244maxresident)k
 # 0inputs+0outputs (0major+5099minor)pagefaults 0swaps
 
-touch 'a.testfile'
-git add .
-git commit -m "Dummy commit"
-rm 'a.testfile'
-git add .
-git commit -m "Dummy commit"
-git push origin dev
+git pull
