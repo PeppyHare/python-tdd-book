7b71d7ae1ea024ca3e08669032b00f616a600dc0
Evan Bluhm <bluhme3@gmail.com>
2018-02-28 10:21:43 -0800

    Automated commit from passing tests. Now on https://www.obeythetestinggoat.com/book/chapter_outside_in.html

--- runtests.remote.sh
+++ runtests.remote.sh
@@ -2,14 +2,18 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR"/venv/bin/activate
 
+fail() {
+	printf "\033[31Not passing tests... :(\033[0m\n"
+}
+
 printf "\033[32mUpdating source from git...\033[0m\n"
-cd "$DIR" || exit 1
-git pull mirror dev
+cd "$DIR" || fail
+git pull mirror dev || fail
 git status
 
 printf "\033[32mRunning Ansible deployment of new source...\033[0m\n"
-time ansible-playbook -i 'localhost,' -c local --extra-vars "ansible_python_interpreter=$DIR/venv/bin/python" -vvvvv "$DIR/deploy_superlists.yml"
+time ansible-playbook -i 'localhost,' -c local --extra-vars "ansible_python_interpreter=$DIR/venv/bin/python" "$DIR/deploy_superlists.yml" || fail
 
 printf "\033[32mRunning full FTs against live server...\033[0m\n"
 export STAGING_SERVER=superlists-staging.peppyhare.uk
-time python "$DIR/manage.py" test --keepdb --failfast --parallel=8 functional_tests
+time python "$DIR/manage.py" test --keepdb --failfast --parallel=8 functional_tests || fail
