a7e9fb74032eeb3d25684d466033fc57a93edbc1
Evan Bluhm <bluhme3@gmail.com>
2018-02-28 10:05:31 -0800

    Automated commit from passing tests. Now on https://www.obeythetestinggoat.com/book/chapter_outside_in.html

--- runtests.remote.sh
+++ runtests.remote.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR"/venv/bin/activate
 
 printf "\033[32mUpdating source from git...\033[0m\n"
+cd "$DIR" || exit 1
 git pull /Git/python-tdd-book.git
 
 printf "\033[32mRunning Ansible deployment of new source...\033[0m\n"
