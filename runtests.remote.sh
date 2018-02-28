3ac4440d34d8efa6016e5bf7bdad9ccae6c91f6b
Evan Bluhm <bluhme3@gmail.com>
2018-02-28 10:46:54 -0800

    Automated commit from passing tests. Now on https://www.obeythetestinggoat.com/book/chapter_outside_in.html

--- runtests.remote.sh
+++ runtests.remote.sh
@@ -17,4 +17,7 @@ time ansible-playbook -i 'localhost,' -c local --extra-vars "ansible_python_inte
 
 printf "\033[32mRunning full FTs against live server...\033[0m\n"
 export STAGING_SERVER=superlists-staging.peppyhare.uk
-time python "$DIR/django/manage.py" test --keepdb --failfast --parallel=8 functional_tests || fail
+while read line; do
+	export "$line"
+done <<< "$DIR/.env"
+time python "$DIR/django/manage.py" test --keepdb --failfast --parallel functional_tests || fail
