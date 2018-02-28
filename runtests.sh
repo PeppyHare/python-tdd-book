3d78ed191280e423c63f1141797bae709607be4e
Evan Bluhm <bluhme3@gmail.com>
2018-02-28 10:22:27 -0800

    Automated commit from passing tests. Now on https://www.obeythetestinggoat.com/book/chapter_outside_in.html

--- runtests.sh
+++ runtests.sh
@@ -60,7 +60,7 @@ branchOff() {
 
 fullTest() {
     cd "$DIR" || fail
-    time ssh ubuntu@superlists.peppyhare.uk "/bin/bash -l /home/ubuntu/GitHub/python-tdd-book/runtests.remote.sh"
+    time ssh ubuntu@superlists.peppyhare.uk "/bin/bash -l /home/ubuntu/GitHub/python-tdd-book/runtests.remote.sh" || fail
 }
 
 commitCode() {
